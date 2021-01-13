a273e78d2e10bc4a03232e8a4681dbf44cdebcf4
tigthor <tigthor1337@gmail.com>
2021-01-13 17:58:56 +0200

    Pushing some changes

---
+++ app/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const greetings = 'Hello My People'
+  return (
+    <div className="App">
+      <h1>My react application</h1>
+    </div>
+  );
+}
+
+export default App;
