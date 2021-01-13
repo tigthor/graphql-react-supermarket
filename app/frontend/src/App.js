070e2be5d5631c2ee4c3fc55e4467c7e3e80cf2e
tigthor <tigthor1337@gmail.com>
2021-01-14 00:49:04 +0200

    Pushing some changes

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -53,7 +53,7 @@ function App() {
 						xl={4}
 						style={{ miHeight: '100%' }}
 					>
-						// {component === 'signin' && <Paper className={classes.paper} elevation={3}><SignIn changeState={changeComponent} /></Paper>}
+						/** {component === 'signin' && <Paper className={classes.paper} elevation={3}><SignIn changeState={changeComponent} /></Paper>} */
 						// {component === 'signup' && <Paper className={classes.paper} elevation={3}><SignUp changeState={changeComponent} /></Paper>}
 					</Grid>
       <hr />
