a911de5c2e46cd1c566162560f8ca9bec1668525
tigthor <tigthor1337@gmail.com>
2021-01-14 03:03:42 +0200

    Pushing some changes

---
+++ app/frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment, Suspense, lazy } from "react";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import login from './components/'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+ 
+        <CssBaseline />
+
+        <Pace color={theme.palette.primary.light} />
+        <Suspense fallback={<Fragment />}>
+          <Switch>
+            <Route path="/c">
+              <LoggedInComponent />
+            </Route>
+          </Switch>
+        </Suspense>
+    </BrowserRouter>
+  );
+}
+
+serviceWorker.register();
+
+export default App;
