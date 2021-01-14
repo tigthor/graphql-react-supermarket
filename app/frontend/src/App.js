59625326ae5074313b1ac9e734c89e674fc62043
tigthor <tigthor1337@gmail.com>
2021-01-14 03:07:20 +0200

    Pushing some changes

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment, Suspense, lazy } from "react";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import login from './components/login/Login'
+import Login from './components/login/Login'
 
 
 function App() {
