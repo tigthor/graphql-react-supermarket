9f815c3a4933bd91c447075617ac4341bd6936ba
tigthor <tigthor1337@gmail.com>
2021-01-14 03:02:42 +0200

    Pushing some changes

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,66 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import {
-	Grid,
-	Box,
-	Divider,
-	Typography,
-	IconButton,
-	Button,
-	Paper,
-	Card
-} from '@material-ui/core';
+import React, { Fragment, Suspense, lazy } from "react";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import 
 
-const courses = [
-  {
-    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
-    author: "Maximilian Schwarzmülller",
-    hours_video: 40.5,
-    number_of_lectures: 490,
-    rating: 4.6,
-    url: "https://codingthesmartway.com/courses/react-complete-guide/"
-  },
-  {
-    title: "Modern React with Redux",
-    author: "Stephen Grider",
-    hours_video: 47.5,
-    number_of_lectures: 488,
-    rating: 4.6,
-    url: "https://codingthesmartway.com/courses/modern-react-with-redux/"
-  },
-  {
-    title: "The Complete React Developer Course (w/ Hooks and Redux)",
-    author: "Andrew Mead",
-    hours_video: 39,
-    number_of_lectures: 200,
-    rating: 4.7,
-    url: "http://codingthesmartway.net/courses/complete-react-web-app-developer/"
-  }
-];
 
 function App() {
-  const greetings = 'Hello My People'
   return (
-    <div className="App">
-      <h1>{greetings}</h1>
-      <h1>List of tutorials</h1>
-      <Grid
-						container
-						justify='center'
-						alignItems='center'
-						xs={12}
-						sm={4}
-						xl={4}
-						style={{ miHeight: '100%' }}
-          >
-          Hello world
-					</Grid>
-      <hr />
-      {courses.map((course) => {
-        return <div>{course.title}</div>
-      })}
-    </div>
+    <BrowserRouter>
+ 
+        <CssBaseline />
+
+        <Pace color={theme.palette.primary.light} />
+        <Suspense fallback={<Fragment />}>
+          <Switch>
+            <Route path="/c">
+              <LoggedInComponent />
+            </Route>
+          </Switch>
+        </Suspense>
+    </BrowserRouter>
   );
 }
+
+serviceWorker.register();
 
 export default App;
